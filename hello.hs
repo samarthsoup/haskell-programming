40fca03e35ad5b59d54e82b9b3c488c6d88d64dc
samarth <samarthkulkarni@icloud.com>
2024-02-14 21:53:04 +0530

    tail recursion

--- hello.hs
+++ hello.hs
@@ -1,7 +1,5 @@
--- use recursion to "re-bind" argument symbols in new scope
-factorial n = if n > 1
-            then n * factorial(n-1)
-            else 1
+factorial n = let loop acc  n' = if n' > 1
+                            then n' * factorial(n'-1)
+                            else 1
+            in loop 1 n
 main = print (factorial 3)
-
--- each recursive call requires its own stack frame
