4953560c0472350ecc1c9bcd491a49337d198d09
samarth <samarthkulkarni@icloud.com>
2024-02-17 22:06:30 +0530

    recursion

---
+++ recursion.hs
@@ -0,0 +1,8 @@
+import Text.XHtml (base)
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = factorial (n-1)
+
+length' :: (Integral b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' [xs]
