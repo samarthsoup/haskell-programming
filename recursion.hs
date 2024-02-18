09cd7f7e7e60a20fd280d7697f59d849a45a21ec
samarth <samarthkulkarni@icloud.com>
2024-02-18 10:43:20 +0530

    maximum element in a list recursive

--- recursion.hs
+++ recursion.hs
@@ -5,3 +5,16 @@ factorial n = factorial (n-1)
 length' :: (Integral b) => [a] -> b
 length' [] = 0
 length' (_:xs) = 1 + length' [xs]
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs) 
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "maximum of empty list"
+maximum'' [x] = x
+maximum'' (x:xs) = max x (maximum'' xs)
