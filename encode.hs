0f653e7ea49d70bc2d91f664fca01f6fcaa9aa22
samarth <samarthkulkarni@icloud.com>
2024-03-05 22:48:52 +0530

    encode

---
+++ encode.hs
@@ -0,0 +1,9 @@
+import Data.Char
+
+encode :: Int -> String -> String
+encode shift msg =
+    let ords = map ord msg
+        shifted = map (+ shift) ords
+    in  map chr shifted
+
+-- encodes a string by shifting it 'shift' times in the unicode table
